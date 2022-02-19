4240dc34c8548b3acdea2ab7157653b4ea8fdbb5
michal <mm.sikorski3@gmail.com>
2022-02-19 22:37:30 +0100

    Add NeurLang/(NeurLang.py and scanner.py) files. In NeurLang there's a NeurLang class. The main class of a code. In scanner we develop a scanner of our language.

---
+++ NeurLang/NeurLang.py
@@ -0,0 +1,21 @@
+import sys
+
+class NeurLang:
+    def __init__(self, test=False):
+        self.start(self.args)
+
+    def start(self, path):
+
+        if self.test == True:
+            pass
+        elif len(path) == 2:
+            self.run(self.load_file(path))
+        else:
+            print("test-case-action")
+
+    def load_file(self, path):
+        with open(path, "r", encoding="utf-8") as f:
+            return f.read()
+
+    def run(self, source):
+        pass
---
+++ NeurLang/scanner.py
@@ -0,0 +1,12 @@
+def is_digit(char):
+    return char >= "0" and char <= "9"
+
+
+def is_alpha(char):
+    return (
+        (char >= "a" and char <= "z") or (char >= "A" and char <= "Z") or char == "_"
+        )
+
+
+def is_alphanumeric(char):
+    return is_digit(char) or is_alpha(char)
